3901f78ee6c3a258e8a57db22e921705c503c26c
MarcosSilva13 <marcosslp45@gmail.com>
2023-03-28 20:41:32 -0300

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_category(name) VALUES('Action');
+INSERT INTO tb_category(name) VALUES('Comedy');
+INSERT INTO tb_category(name) VALUES('Drama');
+INSERT INTO tb_category(name) VALUES('Horror');
+INSERT INTO tb_category(name) VALUES('Sci-fi');
+
+INSERT INTO tb_movie(name, category_id) VALUES('Fast and Furious', 1);
+INSERT INTO tb_movie(name, category_id) VALUES('Avengers', 1);
+INSERT INTO tb_movie(name, category_id) VALUES('Back to the future', 2);
+INSERT INTO tb_movie(name, category_id) VALUES('The Simpsons', 2);
+INSERT INTO tb_movie(name, category_id) VALUES('Forrest Gump', 3);
+INSERT INTO tb_movie(name, category_id) VALUES('Schindlers List', 3);
+INSERT INTO tb_movie(name, category_id) VALUES('It', 4);
+INSERT INTO tb_movie(name, category_id) VALUES('The Conjuring', 4);
+INSERT INTO tb_movie(name, category_id) VALUES('Star Wars', 5);
+INSERT INTO tb_movie(name, category_id) VALUES('Matrix', 5);
