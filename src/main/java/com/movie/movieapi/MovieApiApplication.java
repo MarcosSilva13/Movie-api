1f4a0ba4473f6fffebfc3908d14bdb3560ecb7e1
MarcosSilva13 <marcosslp45@gmail.com>
2023-04-07 15:29:23 -0300

    removendo imports não utilizados

--- src/main/java/com/movie/movieapi/MovieApiApplication.java
+++ src/main/java/com/movie/movieapi/MovieApiApplication.java
@@ -1,17 +1,7 @@
 package com.movie.movieapi;
 
-import com.movie.movieapi.entity.Category;
-import com.movie.movieapi.entity.Movie;
-import com.movie.movieapi.repository.CategoryRepository;
-import com.movie.movieapi.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class MovieApiApplication {
