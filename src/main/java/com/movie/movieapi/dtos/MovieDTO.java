386c907bd023caef50ab1b6f995b3cbdd7c54728
MarcosSilva13 <marcosslp45@gmail.com>
2023-04-16 19:51:42 -0300

    adicionando atributo releaseYear e ajustes

--- src/main/java/com/movie/movieapi/dtos/MovieDTO.java
+++ src/main/java/com/movie/movieapi/dtos/MovieDTO.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.movie.movieapi.entity.Movie;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class MovieDTO {
     private Long id;
 
     @NotBlank(message = "Movie name with invalid format.")
     private String name;
+
+    @NotNull(message = "Movie releaseYear with invalid format.")
+    private Integer releaseYear;
 
     @JsonProperty("Category")
     private CategoryDTO categoryDTO;
@@ -20,6 +24,7 @@ public class MovieDTO {
     public MovieDTO(Movie movie) {
         this.id = movie.getId();
         this.name = movie.getName();
+        this.releaseYear = movie.getReleaseYear();
         categoryDTO = new CategoryDTO(movie.getCategory());
     }
 
@@ -37,6 +42,14 @@ public class MovieDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
     }
 
     public CategoryDTO getCategoryDTO() {
