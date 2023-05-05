5f094fae061ed29ece8c4abbe7e31c54be222d7b
MarcosSilva13 <marcosslp45@gmail.com>
2023-05-05 19:02:58 -0300

    Query para consulta com findByName

--- src/main/java/com/movie/movieapi/repository/MovieRepository.java
+++ src/main/java/com/movie/movieapi/repository/MovieRepository.java
@@ -2,9 +2,14 @@ package com.movie.movieapi.repository;
 
 import com.movie.movieapi.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @Query(value = "SELECT * FROM tb_movie m WHERE m.name like %?%", nativeQuery = true)
+    List<Movie> findByName(String name);
 }
